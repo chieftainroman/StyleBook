44d2ac2fe43f6a5803eb5e9e51264cacf8859ae4
Roman <romanmammadov872@gmail.com>
2026-06-17 10:23:34 -0700

    Switch to PostgreSQL via dj-database-url

--- core/settings.py
+++ core/settings.py
@@ -58,11 +58,14 @@ TEMPLATES = [{
     ]},
 }]
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 STATIC_URL = '/static/'
